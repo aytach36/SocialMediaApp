ec251601946e832d38cdb508d4605d510f82f447
aytach36 <aytchkf@gmail.com>
2024-01-09 21:45:37 +0200

    Styled Tab Labels

---
+++ components/ProfileTabTitle/style.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+import {getFontFamily} from '../../assets/fonts/helper';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/styles/scaling';
+
+const style = StyleSheet.create({
+  title: {
+    color: '#022150',
+    fontFamily: getFontFamily('Inter', '500'),
+    fontSize: scaleFontSize(16),
+    padding: horizontalScale(15),
+  },
+  titleNotFocused: {
+    color: '#79869f',
+    fontFamily: getFontFamily('Inter', '400'),
+  },
+});
+
+export default style;
